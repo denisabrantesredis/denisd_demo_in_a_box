1816db27c54317599455364108fa71334d8ec450
denisabrantesredis <denis.abrantes@redis.com>
2024-10-28 09:58:06 -0500

    Update views.py

--- demos/_admin/home/views.py
+++ demos/_admin/home/views.py
@@ -132,15 +132,16 @@ def create_index(r, prefix, attributes):
 
 def get_jupyter_token():
     token = ''
-    with open(r'/root/.local/share/jupyter/runtime/jpserver-7-open.html', 'r') as f:
+    with open(r'jupyter_token.html', 'r') as f:
         lines = f.readlines()
         for line in lines:
             if line.find('token=') != -1:
                 start = line.index('token=') + 6
-                end = line.index('" />')
+                end = line.index('/>') - 2
                 for idx in range(start, end):
                     token = token + line[idx]
-    return token 
+                break
+        return token
 
 
 # Basic Content
